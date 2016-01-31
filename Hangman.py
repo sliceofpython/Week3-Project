f173d6bfcaf1072ba87e2a191f8ac5d1a7c073dd
Christian Rasmussen <himrasmussen@gmail.com>
2016-01-31 10:46:25 -0800

    Update Hangman.py
    TODO: Make script reject bad input, fix wrong word guess add to wrongGuess

--- Hangman.py
+++ Hangman.py
@@ -10,29 +10,32 @@ import random
 
 def main():
     win = False
-    wordList = ['money', 'snow', 'lime', 'citron', 'police', 'computer', 'ambulance', 'tower', 'education',] #List of words for Hangman
+    wordList = ['money', 'snow', 'lime', 'citron', 'police', 'computer', 
+                'ambulance', 'tower', 'education', 'window'] #List of words for Hangman
     secretWord = wordList[random.randint(0, (len(wordList)-1))] #Selects random word from list
     correctGuess = ''
     for i in secretWord:
         correctGuess += '_ '
     wrongGuess = ''
     wrongGuesses = 0
-    print('Welcome to hangman. You are going to figure out what the secret word is by guessing letters. You can guess wrong 10 times.')
+    print('Welcome.')
     print('Secret word: ' + str(correctGuess))
-    while wrongGuesses <= 9: #and '-' in correctGuess: #Until 10 wrong guesses by user or word guessed
-        guess = input('Guess a letter.\n') #Makes the user guess a letter.
+    while wrongGuesses <= 9 and not win:
+        guess = input('Guess a letter.\n')
         n = -2
         if guess == secretWord:
             win = True
             break
         
         for i in secretWord:
+            # +2 because it has to jump over the whitespaces in correctGuess
             n += 2
             if guess == i:
+                # this replaces the correct letters (correctGuess[i] = guess is prettier but impossible)
                 correctGuess = correctGuess[:n] + guess + correctGuess[n+1:]
         print(correctGuess)
 
-        if guess in wrongGuess or guess == correctGuess:
+        if guess in wrongGuess:
             print('Already guessed.')
         elif guess not in secretWord:
             print('Wrong')
@@ -41,22 +44,23 @@ def main():
             print('Letters not in the word: ' + str(wrongGuess))
             wrongGuesses += 1
             print('Guesses left: ' + str(10 - wrongGuesses))
-
         if '_' not in correctGuess:
+            win = True
             break
 
-    if '_' not in correctGuess or win:
+    if win:
         print('You won!')
     else:
         print('You lost :( \n'
-              'The word was: ' + str(secretWord) + '.')
+              'The word was: ' + str(secretWord))
     while True:
         replay = input('Replay? yes/no')
-        if replay == 'yes':
+        if replay.startswith('y'):
             print('')
             main()
-        elif replay == 'no':
-            print('Goodbye')
+        elif replay.startswith('n'):
+            print('BOB-BIP')
             break
-    
+        else:
+            print('Try again')
 main()
